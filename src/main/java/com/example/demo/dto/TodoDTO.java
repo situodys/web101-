16c71219de3a9dbbe5c1d4ba495f1749fbf10be8
situodys <situodys@gmail.com>
2022-06-29 11:15:41 +0900

    시작

---
+++ src/main/java/com/example/demo/dto/TodoDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.TodoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+    private String id;
+    private String title;
+    private boolean done;
+
+    public TodoDTO(final TodoEntity todoEntity) {
+        this.id = todoEntity.getId();
+        this.title = todoEntity.getTitle();
+        this.done = todoEntity.isDone();
+    }
+}
