f9b73152707c3c619645c1f5cbb37faeee1aa14e
situodys <situodys@gmail.com>
2022-06-30 15:31:35 +0900

    Feat: "FE추가 todo item 삭제"
    delete 프로퍼티를 todoItem의 상위 컴포넌트인 App에서 정의하고
    props를 통해 전달하여 사용
    삭제 로직은 filter()를 통해 타겟 item의 id와 다른 것들만
    새로 생성하는 방법으로 구현

--- src/main/frontend/src/Todo.js
+++ src/main/frontend/src/Todo.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {ListItem, ListItemText,InputBase,Checkbox} from "@material-ui/core";
+import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton} from "@material-ui/core";
+import {DeleteOutlined} from "@material-ui/icons";
 
 class Todo extends React.Component {
     constructor(props) {
         super(props);
         this.state={item: props.item};
+        this.delete = props.delete;
     }
+
+    deleteEventHandler = () => {
+        this.delete(this.state.item)
+    }
+
     render() {
         const item = this.state.item;
         return (
@@ -22,6 +29,15 @@ class Todo extends React.Component {
                         fullWidth={true}
                     />
                 </ListItemText>
+
+                <ListItemSecondaryAction>
+                    <IconButton
+                        aria-label="Delete Todo"
+                        onClick={this.deleteEventHandler}
+                    >
+                        <DeleteOutlined/>
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
 
         );
