86f0bc7413f7b4c2012eb7b729a2007197a539bd
situodys <situodys@gmail.com>
2022-07-01 22:46:12 +0900

    Feat: "[BE] 추가 로그인 인증 기능"
    사용자 관리에 필요한 UserEntity, UserRepository,
    UserService, UserController 생성
    한계
        1. 로그인 상태 확인 및 유지가 불가능하다
        2. 패스워드 암호화되지 않았다

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.UserEntity;
+import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            UserEntity userEntity = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+
+            UserEntity registerUser = userService.create(userEntity);
+
+            UserDTO resUserDTO = UserDTO.builder()
+                    .email(registerUser.getEmail())
+                    .id(registerUser.getId())
+                    .username(registerUser.getUsername())
+                    .build();
+
+            return ResponseEntity.ok().body(resUserDTO);
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity
+                    .badRequest()
+                    .body(responseDTO);
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
+        UserEntity userEntity = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
+
+        if (userEntity != null) {
+            final UserDTO resUserDTO = UserDTO.builder()
+                    .email(userEntity.getEmail())
+                    .id(userEntity.getId())
+                    .build();
+            return ResponseEntity.ok().body(resUserDTO);
+        } else {
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("Login failed")
+                    .build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+}
